8e4de7e47bfe4303ff160a3ff358377a2187e808
str-leshs <0805eslee@gmail.com>
2026-05-08 14:01:53 +0900

    Week 9: Vue Router 연동 및 최종 프로젝트 뼈대 구축

---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script setup>
+</script>
+
+<template>
+  <main class="page">
+    <h1>우리만의 영화 리뷰 사이트</h1>
+    <P>최종 프로젝트 메인 페이지입니다. 환영합니다!</P>
+  </main>
+</template>
+
+<style scoped>
+  .page {
+    padding: 40px;
+    text-align: center;
+  }
+</style>
